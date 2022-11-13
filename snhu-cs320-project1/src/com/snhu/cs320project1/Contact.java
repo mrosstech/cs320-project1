3942015b16a95ba6c3f02c01ebb9029660d8ccf0
mrosstech <mrosstech@gmail.com>
2022-11-13 12:40:14 -0500

    created skeleton for Contact class

--- snhu-cs320-project1/src/com/snhu/cs320project1/Contact.java
+++ snhu-cs320-project1/src/com/snhu/cs320project1/Contact.java
@@ -1,5 +1,49 @@
 package com.snhu.cs320project1;
 
 public class Contact {
-
+	/*
+		TODO: The contact object shall have a required unique contact ID string that cannot be longer than 10 characters. The contact ID shall not be null and shall not be updatable.
+		TODO: The contact object shall have a required firstName String field that cannot be longer than 10 characters. The firstName field shall not be null.
+		TODO: The contact object shall have a required lastName String field that cannot be longer than 10 characters. The lastName field shall not be null.
+		TODO: The contact object shall have a required phone String field that must be exactly 10 digits. The phone field shall not be null.
+		TODO: The contact object shall have a required address field that must be no longer than 30 characters. The address field shall not be null.
+	*/
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private String address;
+	
+	
+	// Class constructors
+	public Contact(String thisId, String thisFirstName, String thisLastName, String thisPhone, String thisAddress) {
+		
+	}
+	
+	public Contact() {
+		
+	}
+	
+	private boolean checkId(String thisId) {
+		
+		return false;
+	}
+	
+	private boolean checkFirstName(String thisId) {
+		
+		return false;
+	}
+	
+	private boolean checkLastName(String thisId) {
+		
+		return false;
+	}
+	private boolean checkPhone(String thisId) {
+		
+		return false;
+	}
+	private boolean checkAddress(String thisId) {
+		
+		return false;
+	}
 }
--- snhu-cs320-project1/src/com/snhu/cs320project1/ContactService.java
+++ snhu-cs320-project1/src/com/snhu/cs320project1/ContactService.java
@@ -1,5 +1,15 @@
 package com.snhu.cs320project1;
 
 public class ContactService {
-
+	/*
+		TODO: The contact service shall be able to add contacts with a unique ID.
+		TODO: The contact service shall be able to delete contacts per contact ID.
+		TODO: The contact service shall be able to update contact fields per contact ID. The following fields are updatable:
+			firstName
+			lastName
+			Number
+			Address
+	 */
+	
+	
 }
