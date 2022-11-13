12fa5c5d5e480992e7a1a47c022444b058e90259
mrosstech <mrosstech@gmail.com>
2022-11-13 15:02:33 -0500

    created test skeletons

---
+++ snhu-cs320-project1/src/com/snhu/cs320project1/ContactServiceTest.java
@@ -0,0 +1,25 @@
+package com.snhu.cs320project1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactServiceTest {
+
+
+	@Test
+	public void testAddContact() {
+		
+	}
+	
+	@Test
+	public void testDeleteContact() {
+		
+	}
+	
+	@Test
+	public void testUpdateContact() {
+		
+	}
+
+}
---
+++ snhu-cs320-project1/src/com/snhu/cs320project1/ContactTest.java
@@ -0,0 +1,54 @@
+package com.snhu.cs320project1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactTest {
+
+	
+	@Test
+	public void testUniqueID() {
+		
+	}
+	
+	@Test
+	public void testFirstNameLength() {
+		
+	}
+	
+	@Test
+	public void testFirstNameNull() {
+		
+	}
+	
+	@Test
+	public void testLastNameLength() {
+		
+	}
+	
+	@Test
+	public void testLastNameNull() {
+		
+	}
+	
+	@Test
+	public void testPhoneLength() {
+		
+	}
+	
+	@Test
+	public void testPhoneNull() {
+		
+	}
+	
+	@Test
+	public void testAddressLength() {
+		
+	}
+	
+	@Test
+	public void testAddressNull() {
+		
+	}
+}
