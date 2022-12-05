e61255ae64c1d7c2add87b49be0ea254f219afcd
mrosstech <mrosstech@gmail.com>
2022-12-04 20:33:52 -0500

    additional comments added for clarity

--- snhu-cs320-project1/src/com/snhu/cs320project1/ContactTest.java
+++ snhu-cs320-project1/src/com/snhu/cs320project1/ContactTest.java
@@ -10,55 +10,82 @@ public class ContactTest {
 	
 	@Test
 	public void testFirstNameLength() {
+		// When 
 		TestContact.setFirstName("Abcdefghijk"); // 11 characters
+		
+		// Then
 		assertTrue("First name lenght greater than 10 characters", TestContact.getFirstName().length() < 11);
 	}
 	
 	@Test
 	public void testFirstNameNull() {
+		// When
 		TestContact.setFirstName(null);
+		
+		// Then
 		assertTrue("First name is null!", TestContact.getFirstName() != null);
 	}
 	
 	@Test
 	public void testLastNameLength() {
+		// When
 		TestContact.setLastName("Abcdefghijk"); // 11 characters
+		
+		// Then
 		assertTrue("First name lenght greater than 10 characters", TestContact.getFirstName().length() < 11);
 	}
 	
 	@Test
 	public void testLastNameNull() {
+		// When
 		TestContact.setLastName(null); // 11 characters
+		
+		// Then
 		assertTrue("Last name is null!", TestContact.getLastName() != null);
 	}
 	
 	@Test
 	public void testPhoneLengthTooLong() {
+		// When
 		TestContact.setPhone("12345678901");
+		
+		// Then
 		assertTrue("Phone number is not being padded properly! Got: " + TestContact.getPhone() + " Expecting: 1234567890", TestContact.getPhone().length() == 10);
 	}
 	
 	@Test
 	public void testPhoneLengthTooShort() {
+		// When
 		TestContact.setPhone("123456789");
+		
+		// Then
 		assertTrue("Phone number is not being padded properly! Got: " + TestContact.getPhone() + " Expecting 1234567890", TestContact.getPhone().length() == 10);
 	}
 	
 	@Test
 	public void testPhoneNull() {
+		// When
 		TestContact.setPhone(null);
+		
+		// Then
 		assertTrue("Phone number is null!", TestContact.getPhone() != null);
 	}
 	
 	@Test
 	public void testAddressLength() {
+		// When
 		TestContact.setAddress("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor");
+		
+		// Then
 		assertTrue("Address is too long!", TestContact.getAddress().length() < 31);
 	}
 	
 	@Test
 	public void testAddressNull() {
+		// When
 		TestContact.setAddress(null);
+		
+		// Then
 		assertTrue("Address is null!", TestContact.getAddress() != null);
 	}
 }
--- snhu-cs320-project1/src/com/snhu/cs320project1/TaskTest.java
+++ snhu-cs320-project1/src/com/snhu/cs320project1/TaskTest.java
@@ -12,50 +12,74 @@ public class TaskTest {
 	
 	@Test
 	public void testTaskNameLengthTooLong() {
+		// When
 		TestTask.setName("Long task name that is longer than 20 characters"); // 47 characters
+		
+		// Then
 		assertTrue("Test: Task NameToo Long, Failed! Expecting: 'Long task name that ' Got: " + TestTask.getName(), TestTask.getName().length() < 21);
 	}
 	
 	@Test
 	public void testTaskNameLengthShort() {
+		// When
 		TestTask.setName("shortTask"); // 9 characters
+		
+		// Then
 		assertTrue("Test: Short task failed, Failed! Expecting: 'shortTask' Got: " + TestTask.getName(), TestTask.getName() == "shortTask");
 	}
 	
 	@Test
 	public void testTaskNameLengthIsTwenty() {
+		// When
 		TestTask.setName("Task is twenty chars"); // 20 characters
+		
+		// Then
 		assertTrue("Test: Exactly 20 chars, Failed! Expecting: 'Task is twenty chars' Got: " + TestTask.getName(), TestTask.getName() == "Task is twenty chars");
 	}
 	
 	@Test
 	public void testTaskNameLengthIsNull() {
+		// When
 		TestTask.setName(null); // null test
+		
+		// Then
 		assertTrue("Test: Null task name, Failed! Expecting: '' Got: " + TestTask.getName(), TestTask.getName() == "");
 	}
 	
 	// Test Task Description Possibilities
 	@Test
 	public void testTaskDescLengthTooLong() {
+		// When
 		TestTask.setDescription("Long task desc that is longer than 50 characters now"); // 52 characters
+		
+		// Then
 		assertTrue("Test: Task Desc Too Long, Failed! Expecting: 'Long task desc that is longer than 50 characters no' Got: " + TestTask.getDescription(), TestTask.getDescription().length() < 51);
 	}
 	
 	@Test
 	public void testTaskDescLengthShort() {
+		// When
 		TestTask.setDescription("Short Task Description"); // 22 characters
+		
+		// Then
 		assertTrue("Test: Short task failed, Failed! Expecting: 'Short Task Description' Got: " + TestTask.getDescription(), TestTask.getDescription() == "Short Task Description");
 	}
 	
 	@Test
 	public void testTaskDescLengthIsFifty() {
+		// When
 		TestTask.setDescription("Task description that is exactly fifty characters!"); // 20 characters
+		
+		// Then
 		assertTrue("Test: Exactly 50 chars, Failed! Expecting: 'Task description that is exactly fifty characters!' Got: " + TestTask.getDescription(), TestTask.getDescription() == "Task description that is exactly fifty characters!");
 	}
 	
 	@Test
 	public void testTaskDescLengthIsNull() {
+		// When
 		TestTask.setDescription(null); // null test
+		
+		// Then
 		assertTrue("Test: Null task description, Failed! Expecting: '' Got: " + TestTask.getDescription(), TestTask.getDescription() == "");
 	}
 
